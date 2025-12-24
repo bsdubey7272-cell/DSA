99fffc838681851f207b6749aadefeee46520d7d
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2025-12-24 05:40:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (18.49%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,4 +1,5 @@
-class Solution {
+//brute force
+/*class Solution {
     public int searchInsert(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             if(nums[i]>=target){
@@ -7,5 +8,25 @@ class Solution {
         }
 
         return nums.length;
+    }
+}
+*/
+//optimal like lower bound
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+      int low=0;
+      int high=nums.length-1;
+      int ans=nums.length;
+      while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]>=target){
+            ans= mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+      }
+      return ans;
     }
 }
