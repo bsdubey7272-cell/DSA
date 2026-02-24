ddd2a1a3b2e90f83c63450bd4406d767504c3514
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-02-24 10:12:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (40.36%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result=new StringBuilder();
+       while(columnNumber>0){
+        columnNumber--;
+        
+        int rem=columnNumber%26;//since column is repeating after 26 
+        result.append((char)('A'+rem));
+        columnNumber=columnNumber/26;
+       } 
+       return result.reverse().toString();
+    }
+}
