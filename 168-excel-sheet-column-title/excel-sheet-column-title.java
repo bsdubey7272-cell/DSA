7b2fc73134c0f7344175ee1d6eccc4b7823d38f4
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-02-24 10:12:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (40.36%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -2,10 +2,10 @@ class Solution {
     public String convertToTitle(int columnNumber) {
         StringBuilder result=new StringBuilder();
        while(columnNumber>0){
-        columnNumber--;
+        columnNumber--;//for adjusting 1 index
         
         int rem=columnNumber%26;//since column is repeating after 26 
-        result.append((char)('A'+rem));
+        result.append((char)(rem+'A'));
         columnNumber=columnNumber/26;
        } 
        return result.reverse().toString();
