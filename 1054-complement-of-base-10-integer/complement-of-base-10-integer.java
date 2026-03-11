0f618cc3e21ebe0f3d5ad0526159590565a0efe4
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-03-11 09:14:26 +0530

    Time: 1 ms (24.44%) | Memory: 42 MB (91.45%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        String ans=Integer.toBinaryString(n);
+        StringBuilder result=new StringBuilder();
+        for(int i=0;i<ans.length();i++){
+            if(ans.charAt(i)=='1'){
+               result.append('0');
+            }
+            else {
+               result.append('1');;
+            }
+        }
+        return  Integer.parseInt(result.toString(),2);
+
+        
+    }
+}
