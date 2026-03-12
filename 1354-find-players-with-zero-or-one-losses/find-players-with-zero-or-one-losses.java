d0cd4e2e909f7b86bb90f76b73e1cdbf1f947993
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-03-12 14:15:53 +0530

    Time: 95 ms (26.55%) | Memory: 166.5 MB (68.47%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+
+        Map<Integer,Integer> lost = new HashMap<>();
+
+        for(int[] it : matches){
+            int lose = it[1];
+            lost.put(lose, lost.getOrDefault(lose,0) + 1);
+        }
+
+        ArrayList<Integer> notlost = new ArrayList<>();
+        ArrayList<Integer> onelost = new ArrayList<>();
+
+        for(int[] it : matches){
+            int won = it[0];
+            int lose = it[1];
+
+            if(lost.get(lose) == 1){
+                onelost.add(lose);
+            }
+
+            if(!lost.containsKey(won)){
+                notlost.add(won);
+                lost.put(won,2);
+            }
+        }
+
+        Collections.sort(notlost);
+        Collections.sort(onelost);
+
+        return Arrays.asList(notlost,onelost);
+    }
+}
