55411f391c77eb496c6b5277305093498178492c
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-03-17 10:09:53 +0530

    Time: 1 ms (98.64%) | Memory: 43.5 MB (80.51%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int evensum=0;
+        int oddsum=0;
+        for(int i=0;i<num.length();i++){
+            if(i%2==0){
+                evensum+=num.charAt(i)-'0';
+            }else oddsum+=num.charAt(i)-'0';
+        }
+        if(evensum==oddsum) return true;
+        return false;
+        
+    }
+}
