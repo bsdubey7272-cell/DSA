ed80b77a6e78e802a6c274f623b657485aa2f69b
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-04-05 10:07:36 +0530

    Time: 8 ms (24.19%) | Memory: 45.4 MB (84.38%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String s) {
+        int moveU=0;
+        int moveD=0;
+        int movesL=0;
+        int movesR=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='L'){
+                movesL++;
+            }else if(s.charAt(i)=='R'){
+                movesR++;
+            }else if(s.charAt(i)=='U'){
+                moveU++;
+            }else{
+                moveD++;
+            }
+            
+
+        }if(moveU==moveD&&movesL==movesR){
+                return true;
+            }
+        return false;
+
+        
+    }
+}
