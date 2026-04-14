6075fd53ef482ab9b136f0d78683505edfc7f8f5
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-04-14 20:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.08%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,6 +1,19 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
-        
+        for (int base = 2; base <= n - 2; base++) {
+            String num = Integer.toString(n, base);
+            
+            int low = 0;
+            int high = num.length() - 1;
+
+            while (low < high) {
+                if (num.charAt(low) != num.charAt(high)) {
+                    return false;
+                }
+                low++;
+                high--;
+            }
+        }
+        return true;
     }
 }
