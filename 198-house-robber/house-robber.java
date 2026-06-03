e9ada229226d0dbb2cf3110f7ac8fbb59f1fb4ee
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-06-04 01:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (54.62%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        java.util.Arrays.fill(dp,-1);
+        
+       
+       
+        return solve(nums,0,n);
+        
+        
+        
+    }
+    int dp[]=new int[101];
+    
+     public int solve(int[]nums,int i,int n){
+         if(i>=n){
+                return 0;
+            }
+        if(dp[i]!=-1){
+            return dp[i];
+
+    }
+           
+            int steal=nums[i]+solve(nums,i+2,n);
+            int skip=solve(nums,i+1,n);
+            return dp[i]=Math.max(steal,skip);
+        }
+}
