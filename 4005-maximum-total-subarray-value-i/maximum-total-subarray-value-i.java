63840faaa0217b64dd36a29391efb2c2b5641944
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-06-09 15:59:30 +0530

    Time: 1 ms (100.00%) | Memory: 62 MB (87.05%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -4,12 +4,8 @@ class Solution {
        int max=Integer.MIN_VALUE;
        int min=Integer.MAX_VALUE;
        for(int i=0;i<nums.length;i++){
-        if(nums[i]>max){
-            max=Math.max(max,nums[i]);
-        }
-        if(nums[i]<min){
-            min=Math.min(min,nums[i]);
-        }
+        max=Math.max(max,nums[i]);
+        min=Math.min(min,nums[i]);
        }
        return (long)(max-min)*k;
     }
