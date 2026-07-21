d030780d0e5e780ee0db9ccd22debb52bf953610
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-07-21 09:58:11 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (51.64%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,71 +1,42 @@
-/*class Solution {
-    public int[] searchRange(int[] nums, int target) {
-    int firstocc=first(nums,target);
-    int lastocc=last(nums,target);
-    return new int[]{lastocc,firstocc};
-    }
-    private int first(int nums[],int target){
-        int ans=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                ans=i;
-            }
-        }
-        return ans;
-    }
-    private int last(int nums[],int target){
-        int ans=-1;
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]==target){
-                ans=i;
-            }
-        }
-        return ans;
-    }
-}
-*/
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-    int firstocc=first(nums,target);
-    int lastocc=last(nums,target);
-    return new int[]{firstocc,lastocc};
+        return new int[]{first(nums,target),last(nums,target)};
+        
     }
-    private int first(int nums[],int target){
+    public int first(int nums[],int target){
+        int left=0;
+        int right=nums.length-1;
+        int ans=-1;
+        while(left<=right){
+            int mid = left+ (right - left) / 2;
+            if(nums[mid]==target){
+                ans=mid;
+                right=mid-1;
+
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else right=mid-1;
+        }
+        return  ans;
+    }
+    public int last(int nums[],int target){
         int ans=-1;
         int low=0;
         int high=nums.length-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }else{
-                high=mid-1;
-            }
-        }
-        
-        return ans;
-    }
-    private int last(int nums[],int target){
-         int ans=-1;
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid = low + (high - low) / 2;
             if(nums[mid]==target){
                 ans=mid;
                 low=mid+1;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }else{
+            else if(nums[mid]>target){
                 high=mid-1;
+
+            }else{
+                low=mid+1;
             }
+
         }
-        
         return ans;
     }
 }
