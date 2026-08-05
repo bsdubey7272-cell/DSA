5f0163ef00e696a23da39bfb1fb66ea743e17854
Balendra Shekhar Dubey <bsdubey7272@gmail.com>
2026-08-05 17:24:41 +0530

    Time: 4 ms (67.26%) | Memory: 43 MB (82.10%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,39 +1,31 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int n = s.length();
-        StringBuilder ans = new StringBuilder();
-        int k = 0;
-
-        while (k < n / 2) {
-            ans.append(s.charAt(k));
-            k++;
-        }
-
-        StringBuilder result = new StringBuilder();
-        int j = n / 2;
-
-        while (j < n) {
-            result.append(s.charAt(j));
-            j++;
-        }
-
-        int cnt1 = 0;
-        int cnt2 = 0;
-
-        for (int i = 0; i < ans.length(); i++) {
-            char ch = Character.toLowerCase(ans.charAt(i));
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        int n=s.length();
+        int cnt1=0;
+        int cnt2=0;
+        for(int i=0;i<n/2;i++ ){
+             if(isvowel(s.charAt(i))){
                 cnt1++;
             }
-        }
 
-        for (int i = 0; i < result.length(); i++) {
-            char ch = Character.toLowerCase(result.charAt(i));
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        }
+        for(int i=n/2;i<n;i++){
+             if(isvowel(s.charAt(i))){
                 cnt2++;
             }
+
         }
 
-        return cnt1 == cnt2;
+        return cnt1==cnt2;
+           
+        
+        
+    }
+
+
+    public boolean isvowel( char s){
+        s=Character.toLowerCase(s);
+
+        return s=='a'||s=='e'||s=='i'||s=='o'||s=='u';
     }
 }
